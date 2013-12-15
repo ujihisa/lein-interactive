03872f866079c6c02684a74ef49be23041751024
ujihisa <ujihisa@gmail.com>
2013-12-14 20:19:01 -0800

    desc + url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject io.github.ujihisa/lein-interactive "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "a leiningen plugin to do tasks interactively"
+  :url "http://github.com/ujihisa/lein-interactive"
   :license {:name "GNU GPL v3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :eval-in-leiningen true)
