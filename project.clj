8c7b8fa438096f39e7b0117cc29933411605e990
ujihisa <ujihisa@gmail.com>
2013-12-15 23:28:51 -0800

    0.1.2dev

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.ujihisa/lein-interactive "0.1.1"
+(defproject io.github.ujihisa/lein-interactive "0.1.2-SNAPSHOT"
   :description "a leiningen plugin to do tasks interactively"
   :url "http://github.com/ujihisa/lein-interactive"
   :license {:name "GNU GPL v3+"
